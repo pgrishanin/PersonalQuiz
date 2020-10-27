24cbdda4b90a645265fe007192588777fcfd2808
Pavel Grishanin <pgrishanin@gmail.com>
2020-10-27 18:30:12 +0300

    Initial Commit

---
+++ PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 26.10.2020.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    var answers: [Answer]?
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты на экране
+    // 4. Избавится от кнопки возврата на предыдущий экран
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.setHidesBackButton(true, animated: false)
+    }
+
+}
